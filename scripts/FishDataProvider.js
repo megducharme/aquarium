999066ede57610df34e960a26539ae87b5749266
Meg Ducharme <ducharme.meg@gmail.com>
2019-11-17 20:10:30 -0600

    added additional code from exercise

---
+++ scripts/FishDataProvider.js
@@ -0,0 +1,18 @@
+/*
+ *  To get you started, here's some properties of Bart.
+ *  You need to add more properties to complete his
+ *  representation as an object. Then add all the other
+ *  fish to the collection.
+ */
+
+const fishCollection = [
+    {
+        name: "Bart",
+        food: ["algae", "crustaceans"]
+    }
+]
+
+// This is new code. Add this.
+export const useFish = () => {
+    return fishCollection
+}
--- scripts/main.js
+++ scripts/main.js
@@ -3,5 +3,12 @@
  *  reference and then invoke it
  */
 import initializeDetailButtonEvents from './dialogs.js'
+import { useFish } from './FishDataProvider.js'
 
 initializeDetailButtonEvents()
+
+const allTheFish = useFish()
+
+for (const fish of allTheFish) {
+    console.log(fish)
+}
